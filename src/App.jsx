63da4fba0ee2aef5c542822aff50ec94599e8035
Rithy Bondeth <rithybondeth999@gmail.com>
2024-08-12 11:42:13 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <>
+      <div className="w-screen h-screen flex justify-center items-center text-xl">Visioncare AI Application</div>
+    </>
+  )
+}
+
